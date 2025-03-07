0494a393f6b4dd1b759db7480971b6bc02c920ef
digas77 <casa.casa.cardozo@gmail.com>
2025-03-07 11:44:14 +0000

    término

--- xadrez.c
+++ xadrez.c
@@ -1,18 +1,36 @@
 #include <stdio.h>
 
-int main() {
-    for (int i = 0; i < 5; i++){
-        printf("Torre foi para a Direita\n");//Imprime a direção do movimento
+void moverTorre(int casas){
+    if (casas > 0){
+        moverTorre(casas - 1);
+        printf("Torre foi para a Direita\n");
     }
+}
+
+int main  (){
+    moverTorre(5);
+    int movimentocompleto = 1;
     
     for (int j = 0; j <= 2; j++){
         printf("Bispo foi para a Diagonal á Direita \n");
     }
 
-    for(int k = 0; k <= 8; k++){
+    for(int k = 0; k <= 7; k++){
         printf("Rainha foi para a Esquerda\n");
     }
+
+    while (movimentocompleto--)
+    {
+        for (int i = 0; i <= 1; i++)
+        {
+            printf("Cavalo foi para Cima\n");
+        }
+        printf("Cavalo foi para a Direita\n");
+    }
     
+
+
+
 
     return 0;
 }
